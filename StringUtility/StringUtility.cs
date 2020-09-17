cb20b241a2f0dcbd8ff8b5283cc030639f19184c
Alexander Egeberg <55107481+AlexanderEgeberg@users.noreply.github.com>
2020-09-17 14:44:11 +0200

    kom nu

---
+++ StringUtility/StringUtility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using System.Xml.Serialization;
+
+namespace StringUtility
+{
+    public class StringUtility
+    {
+        public int CountOccurences(string stringToCheck, string stringToFind)
+        {
+            if (stringToCheck == null || stringToCheck == "")
+            {
+                return -1;
+            }
+            var stringToCheckLower = stringToCheck.ToLower();
+            var stringAsCharArray = stringToCheckLower.ToCharArray();
+            var stringToCheckForAsChar = stringToFind.ToCharArray()[0];
+            var occuranceCount = 0;
+
+            foreach (var c in stringAsCharArray)
+            {
+                if (c == stringToCheckForAsChar)
+                {
+                    occuranceCount++;
+                }
+            }
+
+            return occuranceCount;
+        }
+        
+    }
+}
